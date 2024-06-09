1bb7a187b32b0fb1704fd99a534e0d9ffa307734
nino <dankozero111@gmail.com>
2024-06-09 23:41:49 +0300

    [add]: home page component with filters and job section

---
+++ src/pages/home/ui/Home.tsx
@@ -0,0 +1,11 @@
+import { JobSection } from '@widgets/JobSection';
+import { Filter } from '@widgets/Filter';
+
+export const Home = () => {
+  return (
+    <>
+      <Filter />
+      <JobSection />
+    </>
+  );
+};
