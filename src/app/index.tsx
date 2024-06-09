3ca886c85910436f210e570bdea0e645bd0a72ed
nino <dankozero111@gmail.com>
2024-06-09 23:40:34 +0300

    [add]: main layout with routing and rtk store provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,36 @@
+import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { store } from '@store/store';
+import { Home } from '@pages/home';
+import { Job } from '@pages/job';
+import { Layout } from '@widgets/Layout';
+import { Error } from '@widgets/Error';
+
+import cn from './style/index.module.css';
+
 const App = () => {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Home /> },
+        {
+          path: '/:id',
+          element: <Job />,
+          errorElement: <Error msg="Some problem" />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <Provider {...{ store }}>
+      <div className={cn.wrapper}>
+        <RouterProvider {...{ router }}></RouterProvider>
+      </div>
+    </Provider>
+  );
 };
 
 export default App;
