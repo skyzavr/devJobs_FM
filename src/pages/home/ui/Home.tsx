4e0e99485b1aa45dc2e25349e99c0d5b2b736837
nino <dankozero111@gmail.com>
2024-06-20 00:54:51 +0300

    [add]: add pagination to the main screen

--- src/pages/home/ui/Home.tsx
+++ src/pages/home/ui/Home.tsx
@@ -1,11 +1,13 @@
 import { JobSection } from '@widgets/JobSection';
 import { Filter } from '@widgets/Filter';
+import { Pagination } from '@widgets/pagination';
 
 export const Home = () => {
   return (
     <>
       <Filter />
       <JobSection />
+      <Pagination />
     </>
   );
 };
